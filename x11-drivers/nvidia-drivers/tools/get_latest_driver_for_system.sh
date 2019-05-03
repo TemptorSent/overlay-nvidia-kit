e3a1a0467f564a2a805dd89580f1495934d361de
Chris A. Giorgi <chrisgiorgi@gmail.com>
2019-05-03 06:04:21 +0000

    nvidia-drivers: Fixup for WIP tools/get_latest_driver_for_system.sh.

--- x11-drivers/nvidia-drivers/tools/get_latest_driver_for_system.sh
+++ x11-drivers/nvidia-drivers/tools/get_latest_driver_for_system.sh
@@ -1,4 +1,6 @@
-IFS="\n"
-for pciid in $(sh get_system_pciids.sh  --with-class=03 10de) ; do
-	sh get_latest_driver_for_pciid.sh "${pciid#* }";
+OIFS=$IFS
+IFS="\n"; for pciid in $(sh get_system_pciids.sh  --with-class=03 10de) ; do
+	IFS=$OIFS
+	mydriver="$(sh get_latest_driver_for_pciid.sh ${pciid#* })"
+	[ -n "${mydriver}" ] && printf -- '%s\n' "${mydriver}"
 done
